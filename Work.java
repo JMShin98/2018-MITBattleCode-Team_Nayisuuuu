8f62a4608b1fcc5eb22261b6247535727cb80f04
coolsky <coolsky@d-10-18-159-14.dhcp4.washington.edu>
2018-01-12 01:38:55 -0800

    Updated Task enums and implemented run() a little.

--- Work.java
+++ Work.java
@@ -1,11 +1,12 @@
 import bc.*;
 import java.util.*;
-public class Work{
+public class Work {
 	private static Work instance;
-	private Map<Unit, WorkTask> tasks;
+	private Map<Unit, Task> tasks;
 	
 	public Work() {
 	}
+	
 	public static Work instance() {
 		if (instance == null) {
 			instance = new Work();
@@ -13,54 +14,83 @@ public class Work{
 		return instance;
 	}
 	
-	public void run() {}
+	public void run() {
+		for (Unit u : tasks.keySet()) {
+			Task w = tasks.get(u);
+			switch(w.taskType) {
+				case Idle:
+				case Harvest: harvest(w.location);
+				case Blueprint:
+				case Build:
+					// need to figure out what units to build
+					
+					
+					build(u, w.location); // 
+				case Repair: repair(w.location);
+			}
+			
+		}
+	}
 	
-	public boolean Harvest(Location location) {
+	public boolean harvest(Location location) {
 		return false;
 	}
-	public boolean Build(UnitType type, Location location) {
+	
+	public boolean build(UnitType type, Location location) {
 		Unit worker = getIdleWorker();
 		if (worker != null) {
-			tasks.put(worker, new WorkTask(WorkTaskType.Blueprint, location));
+			tasks.put(worker, new Task(TaskType.Blueprint, location));
 			return true;
 		} else {
 			return false;
 		}
 	}
-	public boolean Repair(Location location) {
+	public boolean repair(Location location) {
 		return false;
 	}
 	
+	// returns the first worker that's idle
 	private Unit getIdleWorker() {
 		List<Unit> workers = Units.instance().units.get(UnitType.Worker);
 		for (Unit worker: workers) {
 			if (worker.location().isOnPlanet(Planet.Earth)
-					&& tasks.get(worker).type == WorkTaskType.Idle) {
+					&& tasks.get(worker).taskType == TaskType.Idle) {
 				return worker;
 			}
 		}
 		return null;
 	}
 	
-	private class WorkTask {
-		public WorkTaskType type;
+	private class Task {
+		public TaskType taskType;
 		public Location location;
-		WorkTask(WorkTaskType type, Location location) {
-			this.type = type;
+		public UnitType unitType;
+		
+		// Constructor for Harvest, Blueprint, and Repair
+		Task(TaskType type, Location location) {
+			this.taskType = type;
 			this.location = location;
 		}
+		
+		// Constructor for Build
+		Task(TaskType taskType, Location location, UnitType unitType) {
+			this.taskType = taskType;
+			this.location = location;
+			this.unitType = unitType;
+		}
 	}
-	private enum WorkTaskType {
+	
+	private enum TaskType {
 		Idle(0),
 		Harvest(1),
 		Blueprint(2),
 		Build(3),
 		Repair(4);
 		private final int value;
-		WorkTaskType() {
+		TaskType() {
 			this.value = 0;
 		}
-		WorkTaskType(int value) {
+		TaskType(int value) {
 			this.value = value;
 		}
 	}
