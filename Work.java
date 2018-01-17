9d052043ae619107a91f88af7b1ab9b085005436
Chahyon Ku <chahyon1998@gmail.com>
2018-01-16 17:19:04 -0800

    Basic Work functions
    finished

--- Work.java
+++ Work.java
@@ -2,9 +2,10 @@ import bc.*;
 import java.util.*;
 public class Work {
 	private static Work instance;
-	private Map<Unit, Task> tasks;
+	private Map<Integer, Task> tasks;
 	
 	public Work() {
+		tasks = new HashMap<>();
 	}
 	
 	public static Work instance() {
@@ -15,38 +16,85 @@ public class Work {
 	}
 	
 	public void run() {
-		for (Unit u : tasks.keySet()) {
-			Task w = tasks.get(u);
-			switch(w.taskType) {
-				case Idle:
-				case Harvest: harvest(w.location);
-				case Blueprint:
-				case Build:
-					// need to figure out what units to build
-					
-					
-					build(u, w.location); // 
-				case Repair: repair(w.location);
+		for (int id : tasks.keySet()) {
+			if (!Player.gc().canSenseUnit(id))
+				tasks.remove(id);
+			
+			Unit worker = Player.gc().unit(id);
+			Task task = tasks.get(id);
+			Direction direction = worker.location().mapLocation().directionTo(task.mapLocation);
+			
+			if (worker.workerHasActed() == 0) {
+				continue;
+			} else if (!worker.location().mapLocation().isAdjacentTo(task.mapLocation)) {
+				Move.instance().move(worker, task.mapLocation);
+			} else if (task.taskType == TaskType.Harvest) {
+				if (Player.gc().karboniteAt(task.mapLocation) == 0) {
+					tasks.remove(id);
+					continue;
+				}
+				if (worker.workerHasActed() != 0
+						&& Player.gc().canHarvest(id, direction)) {
+					Player.gc().harvest(id, direction);
+				}
+			} else if (task.taskType == TaskType.Blueprint) {
+				if (worker.workerHasActed() != 0
+						&& Player.gc().canBlueprint(id, task.unitType, direction)) {
+					Player.gc().blueprint(id, task.unitType, direction);
+					tasks.put(id, new Task(TaskType.Build, task.mapLocation));
+				}
+			} else if (task.taskType == TaskType.Build) {
+				Unit structure = Player.gc().senseUnitAtLocation(task.mapLocation);
+				if (structure.structureIsBuilt() != 0) {
+					tasks.remove(id);
+					continue;
+				}
+				if (worker.workerHasActed() != 0
+						&& Player.gc().canBuild(id, structure.id())) {
+					Player.gc().build(id, structure.id());
+				}
+			} else if (task.taskType == TaskType.Repair) {
+				Unit structure = Player.gc().senseUnitAtLocation(task.mapLocation);
+				if (structure.health() == structure.maxHealth()) {
+					tasks.remove(id);
+					continue;
+				}
+				if (worker.workerHasActed() != 0
+						&& Player.gc().canRepair(id, structure.id())) {
+					Player.gc().repair(id, structure.id());
+				}
 			}
 			
 		}
 	}
 	
-	public boolean harvest(Location location) {
-		return false;
-	}
-	
-	public boolean build(UnitType type, Location location) {
+	public boolean harvest(MapLocation mapLocation) {
 		Unit worker = getIdleWorker();
 		if (worker != null) {
-			tasks.put(worker, new Task(TaskType.Blueprint, location));
+			tasks.put(worker.id(), new Task(TaskType.Harvest, mapLocation));
 			return true;
 		} else {
 			return false;
 		}
 	}
-	public boolean repair(Location location) {
-		return false;
+	
+	public boolean build(UnitType type, MapLocation mapLocation) {
+		Unit worker = getIdleWorker();
+		if (worker != null) {
+			tasks.put(worker.id(), new Task(TaskType.Blueprint, type, mapLocation));
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public boolean repair(MapLocation mapLocation) {
+		Unit worker = getIdleWorker();
+		if (worker != null) {
+			tasks.put(worker.id(), new Task(TaskType.Repair, mapLocation));
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	// returns the first worker that's idle
@@ -63,20 +111,18 @@ public class Work {
 	
 	private class Task {
 		public TaskType taskType;
-		public Location location;
+		public MapLocation mapLocation;
 		public UnitType unitType;
-		
-		// Constructor for Harvest, Blueprint, and Repair
-		Task(TaskType type, Location location) {
-			this.taskType = type;
-			this.location = location;
-		}
-		
-		// Constructor for Build
-		Task(TaskType taskType, Location location, UnitType unitType) {
+
+		// Constructor for Blueprint
+		Task(TaskType taskType, UnitType unitType, MapLocation mapLocation) {
 			this.taskType = taskType;
-			this.location = location;
+			this.mapLocation = mapLocation;
 			this.unitType = unitType;
+		}
+		// Constructor for Harvest, Build, and Repair
+		Task(TaskType type, MapLocation mapLocation) {
+			this(type, null, mapLocation);
 		}
 	}
 	
